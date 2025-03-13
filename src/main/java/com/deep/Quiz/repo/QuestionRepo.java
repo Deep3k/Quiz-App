d0821791bcdfaee86ba9e6093b8e5dc4260a9cd9
Deep Khanvilkar <deep.n.khanvilkar@slrtce.in>
2025-03-13 19:10:15 +0530

    first commit

---
+++ src/main/java/com/deep/Quiz/repo/QuestionRepo.java
@@ -0,0 +1,11 @@
+package com.deep.Quiz.repo;
+
+import com.deep.Quiz.entity.QuizQuestion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuestionRepo extends JpaRepository<QuizQuestion,Long> {
+
+
+}
